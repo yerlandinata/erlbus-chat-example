5cd4601cc40c0367a38ac005d10d1e9884c4ae19
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-12-06 01:48:04 +0700

    add change chatroom in UI and clean up some code repetition

--- examples/chat/src/chat_cowboy_ws_handler.erl
+++ examples/chat/src/chat_cowboy_ws_handler.erl
@@ -12,33 +12,42 @@
 
 -record(state, {name, handler, chatroom}).
 
+to_string(Value) when is_binary(Value) -> binary_to_list(Value);
+to_string(Value) -> Value.
+concat(List) ->
+  list_to_binary(lists:flatten(lists:map(fun to_string/1, List))).
+
 %% API
 
 init(_, _Req, _Opts) ->
   {upgrade, protocol, cowboy_websocket}.
 
+send_chat(Chatroom, Sender, Message) ->
+  ebus:pub(Chatroom, {Sender, Message}).
+
 websocket_init(_Type, Req, _Opts) ->
   % Create the handler from our custom callback
   Handler = ebus_proc:spawn_handler(fun chat_erlbus_handler:handle_msg/2, [self()]),
   ebus:sub(Handler, ?PUBLIC_CHATROOM),
+  ebus:pub(?PUBLIC_CHATROOM, {<<"Server">>, concat([get_name(Req), " joined ", ?PUBLIC_CHATROOM])}),
   {ok, Req, #state{name = get_name(Req), handler = Handler, chatroom = ?PUBLIC_CHATROOM}, ?TIMEOUT}.
 
-update_state(State, [Type, Payload]) when Type =:= <<"name">> -> 
-  % ebus:pub(?DEBUG, "Name change from " ++ State#state.name ++ " to " ++ Payload),
-  #state{name = Payload, handler = State#state.handler, chatroom = State#state.chatroom};
-update_state(State, [Type, Payload]) when Type =:= <<"chatroom">> -> 
-  % ebus:pub(?DEBUG, State#state.name ++ " left " ++ State#state.chatroom ++ " and then joined chatroom_" ++ Payload),
+handle_chat(_, Req, State, [Type, Payload]) when Type =:= <<"name">> -> 
+  send_chat(State#state.chatroom, <<"Server">>, concat([State#state.name, " changed name to ", Payload])),
+  {ok, Req, #state{name = Payload, handler = State#state.handler, chatroom = State#state.chatroom}};
+handle_chat(_, Req, State, [Type, Payload]) when Type =:= <<"chatroom">> -> 
   ebus:unsub(State#state.handler, State#state.chatroom),
   ebus:sub(State#state.handler, "chatroom_" ++ Payload),
-  #state{name = State#state.name, handler = State#state.handler, chatroom = "chatroom_" ++ Payload};
-update_state(State, _) -> 
-  ebus:pub(?DEBUG, "aa message has been sent"),
-  State.
+  send_chat(State#state.chatroom, <<"Server">>, concat([State#state.name, " left this chatroom"])),
+  send_chat(concat(["chatroom_", Payload]), <<"Server">>, concat([State#state.name, " joined ", concat(["chatroom_", Payload])])),
+  {ok, Req, #state{name = State#state.name, handler = State#state.handler, chatroom = concat(["chatroom_", Payload])}};
+handle_chat(Msg, Req, State, _) -> 
+  send_chat(State#state.chatroom, State#state.name, Msg),
+  {ok, Req, State}.
 
 websocket_handle({text, Msg}, Req, State) ->
-  ebus:pub(State#state.chatroom, {State#state.name, Msg}),
-  ebus:pub(?DEBUG, string:split(Msg, "|")),
-  {ok, Req, update_state(State, string:split(Msg, "|"))};
+  ebus:pub(?DEBUG, {text, Msg}),
+  handle_chat(Msg, Req, State, string:split(Msg, "|"));
 websocket_handle(_Data, Req, State) ->
   {ok, Req, State}.
 
@@ -50,6 +59,7 @@ websocket_info(_Info, Req, State) ->
 websocket_terminate(_Reason, _Req, State) ->
   % Unsubscribe the handler
   ebus:unsub(State#state.handler, State#state.chatroom),
+  send_chat(State#state.chatroom, <<"Server">>, concat([State#state.name, " left this chatroom"])),
   ok.
 
 %% Private methods
